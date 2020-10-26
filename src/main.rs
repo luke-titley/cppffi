3ac91afd6f2adb28c7d05070475d2273bd16e803
Luke Titley <luke@titley.com>
2020-10-26 13:54:27 +0100

    Added stub code to iterate over the classes in a header file

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,30 @@
 type Result<R> = std::result::Result<R, std::string::String>;
 
 //------------------------------------------------------------------------------
+fn handle_class(entity: clang::Entity) {
+    println!("{}", entity.get_display_name().unwrap())
+}
+
+//------------------------------------------------------------------------------
 fn doit() -> Result<()> {
     let state = clang::Clang::new()?;
     let index = clang::Index::new(&state, true, true);
+
+    let parser = index.parser("example_class.h");
+
+    let translation_unit = parser.parse().unwrap();
+
+    // Loop over the header file exporting everything that is of interest
+    translation_unit.get_entity().visit_children(|entity, parent| {
+        match entity.get_kind() {
+           // We want to output classes
+           clang::EntityKind::ClassDecl => handle_class(entity),
+
+           // Ignore everything else
+           _ => (),
+        }
+        clang::EntityVisitResult::Continue
+    });
 
     Ok(())
 }
