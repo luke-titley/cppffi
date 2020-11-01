34ea2e4dd8dddef724bfb85c3b3a0b68d266c6aa
Luke Titley <luke@titley.com>
2020-11-01 23:53:07 +0100

    Make the static assert for size, multi-line

--- src/gen/constructor.rs
+++ src/gen/constructor.rs
@@ -15,8 +15,10 @@ void {{class}}_{{outer_method}}({{class}} * self{{comma}}{{params}});
 ";
 
 static BODY_TEMPLATE: &'static str = "
-static_assert(sizeof({{class}}) == sizeof({{{cpp_class}}}), \"Miss matching size for {{{cpp_class}}}\");
-static_assert(alignof({{class}}) == alignof({{{cpp_class}}}), \"Miss matching alignment for {{{cpp_class}}}\");
+static_assert(sizeof({{class}}) == sizeof({{{cpp_class}}}),
+             \"Mismatching size for {{{cpp_class}}}\");
+static_assert(alignof({{class}}) == alignof({{{cpp_class}}}),
+             \"Mismatching alignment for {{{cpp_class}}}\");
 
 void {{class}}_{{outer_method}}({{class}} * self{{comma}}{{params}})
 { {{{types}}}
